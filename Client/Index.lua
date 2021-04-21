60bdc8ad58b7ac756ce4f750df430ae024fb7730
Gabriel T. Nardy <gtnardy@gmail.com>
2021-04-21 09:55:50 -0300

    Removed spaces and fixed some minor bugs

--- Client/Index.lua
+++ Client/Index.lua
@@ -3,20 +3,18 @@ KillHUDUI = nil
 
 -- Creates a WebUI for the Inventory when the package loads
 Package:Subscribe("Load", function()
-    KillHUDUI = WebUI("KillHUD", "file:///UI/index.html")
+	KillHUDUI = WebUI("KillHUD", "file:///UI/index.html")
 end)
 
 -- Destroys the WebUI when the package unloads
 Package:Subscribe("Unload", function()
-    KillHUDUI:Destroy()
+	KillHUDUI:Destroy()
 end)
 
 -- When a character takes damage, checks if I was the causer and displays it on the screen
 Character:Subscribe("TakeDamage", function(charact, damage, bone, type, from, instigator)
-    -- If I was not the causer, just ignore it
-    if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
-
-	Client:SendChatMessage("Hit: " .. bone)
+	-- If I was not the causer, just ignore it
+	if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
 
 	Sound(Vector(0, 0, 0), "NanosWorld::A_Hit_Feedback", true)
 
@@ -24,30 +22,27 @@ Character:Subscribe("TakeDamage", function(charact, damage, bone, type, from, in
 		Sound(Vector(0, 0, 0), "NanosWorld::A_Headshot_Feedback", true)
 	end
 
-    KillHUDUI:CallEvent("AddScore", {damage, "enemy_hit", "ENEMY HIT", true})
+	KillHUDUI:CallEvent("AddScore", {damage, "enemy_hit", "ENEMY HIT", true})
 end)
 
 -- When a character dies, check if I was the last one to do damage on him and displays on the screen as a kill
 Character:Subscribe("Death", function(charact, last_damage_taken, last_bone_damaged, damage_type_reason, hit_from_direction, instigator)
-    if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
+	if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
 
-    local player = charact:GetPlayer()
+	local player = charact:GetPlayer()
 
-    local name = "BOT"
+	local name = "BOT"
 
-    -- Attempts to get the player's name (if possessed by one)
-    if (player) then
-        name = player:GetName()
-    end
-
-    -- Gets the lat hit bone and check if it was a Headshot
-    local is_headshot = last_bone_damaged == "head" or last_bone_damaged == "neck_01"
-
-	Sound(Vector(0, 0, 0), "NanosWorld::A_Kill_Feedback", true)
-
-	if (is_headshot) then
-		Sound(Vector(0, 0, 0), "NanosWorld::A_Headshot_Feedback", true)
+	-- Attempts to get the player's name (if possessed by one)
+	if (player) then
+		name = player:GetName()
 	end
 
-    KillHUDUI:CallEvent("AddKill", {name, is_headshot})
+    -- Plays a sound kill feedback
+	Sound(Vector(0, 0, 0), "NanosWorld::A_Kill_Feedback", true)
+
+	-- Gets the lat hit bone and check if it was a Headshot
+	local is_headshot = last_bone_damaged == "head" or last_bone_damaged == "neck_01"
+
+	KillHUDUI:CallEvent("AddKill", {name, is_headshot})
 end)
--- Client/UI/index.js
+++ Client/UI/index.js
@@ -4,88 +4,88 @@ var total_score = 0;
 var total_score_current = 0;
 
 Events.Subscribe("AddScore", function(score, type_id, label, use_current) {
-    total_score += score;
+	total_score += score;
 
-    // Displays all DOM elements
-    DisplayHUD();
+	// Displays all DOM elements
+	DisplayHUD();
 
-    // If should increment current score item instead of creating a new one
-    if (use_current)
-    {
-        // Try to find if there is already a score item
-        let score_feed_item_value = $(`.${type_id} .score_value_item`);
+	// If should increment current score item instead of creating a new one
+	if (use_current)
+	{
+		// Try to find if there is already a score item
+		let score_feed_item_value = $(`.${type_id} .score_value_item`);
 
-        // If a score item is found, then update it and return, otherwise it will keep on and will create a new one
-        if (score_feed_item_value.length)
-        {
-            score_feed_item_value.html(parseInt(score_feed_item_value.html()) + score);
-            return;
-        }
-    }
+		// If a score item is found, then update it and return, otherwise it will keep on and will create a new one
+		if (score_feed_item_value.length)
+		{
+			score_feed_item_value.html(parseInt(score_feed_item_value.html()) + score);
+			return;
+		}
+	}
 
-    // Create a new score item and prepend it on the list
-    let score_feed_item = $(`<span class='score_feed_item ${type_id}'>${label} +<span class='score_value_item'>${score}</span></span>`);
-    $("#score_feed").prepend(score_feed_item);
+	// Create a new score item and prepend it on the list
+	let score_feed_item = $(`<span class='score_feed_item ${type_id}'>${label} +<span class='score_value_item'>${score}</span></span>`);
+	$("#score_feed").prepend(score_feed_item);
 });
 
 Events.Subscribe("AddKill", function(name, is_headshot) {
-    // Adds 20 score for killing
-    total_score += 20;
+	// Adds 20 score for killing
+	total_score += 20;
 
-    // Displays the killed name
-    $("#death_name").html(`${name} +20`);
+	// Displays the killed name
+	$("#death_name").html(`${name} +20`);
 
-    // If it was headshot, displays the red skull, otherwise displays the white
-    let death_count_white = $(`<span class='death_count ${is_headshot ? "death_count_red" : "death_count_white"}'>`);
-    $("#death_counts").prepend(death_count_white);
+	// If it was headshot, displays the red skull, otherwise displays the white
+	let death_count_white = $(`<span class='death_count ${is_headshot ? "death_count_red" : "death_count_white"}'>`);
+	$("#death_counts").prepend(death_count_white);
 
-    // Displays all DOM elements
-    DisplayHUD();
+	// Displays all DOM elements
+	DisplayHUD();
 });
 
 // Resets all animations and displays the HUD, also resets the timers and creates a new one to hide it in 4 seconds
 function DisplayHUD() {
-    $("#death_counts").stop(true, true).show();
-    $("#death_name").stop(true, true).show();
-    $("#score_feed").stop(true, true).show();
-    $("#total_score").stop(true, true).show();
+	$("#death_counts").stop(true, true).show();
+	$("#death_name").stop(true, true).show();
+	$("#score_feed").stop(true, true).show();
+	$("#total_score").stop(true, true).show();
 
-    if (timeout_hud)
-        clearTimeout(timeout_hud);
+	if (timeout_hud)
+		clearTimeout(timeout_hud);
 
-    timeout_hud = setTimeout(ResetHUD, 4000);
+	timeout_hud = setTimeout(ResetHUD, 4000);
 
-    if (interval_score)
-        clearInterval(interval_score);
+	if (interval_score)
+		clearInterval(interval_score);
 
-    interval_score = setInterval(UpdateHUD, 50);
+	interval_score = setInterval(UpdateHUD, 50);
 }
 
 // Resets the HUD, i.e. hides everything with animations and resets all data
 function ResetHUD() {
-    $("#death_counts").fadeOut(500, function() {
-        $("#death_counts").html("");
-    });
+	$("#death_counts").fadeOut(500, function() {
+		$("#death_counts").html("");
+	});
 
-    $("#death_name").fadeOut(500, function() {
-        $("#death_name").html("");
+	$("#death_name").fadeOut(500, function() {
+		$("#death_name").html("");
 
-        $("#score_feed").fadeOut(500, function() { $("#score_feed").html(""); });
-        $("#total_score").fadeOut(500);
-    });
+		$("#score_feed").fadeOut(500, function() { $("#score_feed").html(""); });
+		$("#total_score").fadeOut(500);
+	});
 
-    clearInterval(interval_score);
-    interval_score = null;
+	clearInterval(interval_score);
+	interval_score = null;
 
-    timeout_hud = null;
-    total_score = 0;
-    total_score_current = 0;
+	timeout_hud = null;
+	total_score = 0;
+	total_score_current = 0;
 }
 
 // Updates the HUD, i.e. updates the current displayed score in the screen
 function UpdateHUD() {
-    let interp_pace = Math.max((total_score - total_score_current) / 2, 1);
+	let interp_pace = Math.max((total_score - total_score_current) / 2, 1);
 
-    total_score_current = parseInt(Math.min(total_score, total_score_current + interp_pace));
-    $("#total_score").html(total_score_current);
+	total_score_current = parseInt(Math.min(total_score, total_score_current + interp_pace));
+	$("#total_score").html(total_score_current);
 }
