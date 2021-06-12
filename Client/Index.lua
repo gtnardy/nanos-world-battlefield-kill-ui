7011fdc5c61157b5a3d8083018d31c6a90c1f1ed
Gabriel T. Nardy <gtnardy@gmail.com>
2021-06-12 07:19:38 -0300

    Added Kill Feed; Custom Configuration; And several improvements

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,27 +1,60 @@
--- Stores the UI Instance
-KillHUDUI = nil
+-- Spawns the UI Instance
+KillHUDUI = WebUI("KillHUD", "file:///UI/index.html")
 
--- Creates a WebUI for the Inventory when the package loads
-Package:Subscribe("Load", function()
-	KillHUDUI = WebUI("KillHUD", "file:///UI/index.html")
-end)
+KillHUDUIConfiguration = {
+	enable_autoscore = true,
+	kill_score = 20,
+	headshot_score = 20,
+	current_damage_id = 1,
+}
 
--- Destroys the WebUI when the package unloads
-Package:Subscribe("Unload", function()
-	KillHUDUI:Destroy()
-end)
+-- List of spawned HitMarks
+HitMarks = {}
 
--- When a character takes damage, checks if I was the causer and displays it on the screen
+-- Helper for adding a Kill to the screen (skull)
+function AddKill(name, is_headshot, score)
+	KillHUDUI:CallEvent("AddKill", { name, is_headshot, score })
+end
+
+Events:Subscribe("AddKill", AddKill)
+
+-- Helper for adding score to the screen
+function AddScore(score, id, label, use_current)
+	KillHUDUI:CallEvent("AddScore", {score, id, label, use_current})
+end
+
+Events:Subscribe("AddScore", AddScore)
+
 Character:Subscribe("TakeDamage", function(character, damage, bone, type, from, instigator)
 	local local_player = NanosWorld:GetLocalPlayer()
-	
-	-- If I was damaged, play Hit Taken sound
+
+	-- If I was damaged, play Hit Taken sound and displays a Hit Mark
 	if (character:GetPlayer() == local_player) then
 		Sound(Vector(), "NanosWorld::A_HitTaken_Feedback", true)
+		KillHUDUIConfiguration.current_damage_id = KillHUDUIConfiguration.current_damage_id + 1
+
+		local location = Vector()
+		if (instigator) then
+			local character_instigator = instigator:GetControlledCharacter()
+
+			if (character_instigator) then
+				location = character_instigator:GetLocation()
+			end
+		end
+
+		table.insert(HitMarks, { id = KillHUDUIConfiguration.current_damage_id, cooldown = 2000, location = location })
+		return
 	end
 
 	-- If I was the causer, adds score
 	if (instigator == local_player) then
+		local local_character = local_player:GetControlledCharacter()
+
+		if (local_character and local_character == character) then
+			return
+		end
+
+		-- Play Hit audio feedback
 		Sound(Vector(), "NanosWorld::A_Hit_Feedback", true)
 
 		-- Headshot sound effect
@@ -29,28 +62,70 @@ Character:Subscribe("TakeDamage", function(character, damage, bone, type, from, 
 			Sound(Vector(), "NanosWorld::A_Headshot_Feedback", true)
 		end
 
-		KillHUDUI:CallEvent("AddScore", {damage, "enemy_hit", "ENEMY HIT", true})
+		-- If we should add score, or other package will do it
+		if (KillHUDUIConfiguration.enable_autoscore) then
+			AddScore(damage, "enemy_hit", "ENEMY HIT", true)
+		end
 	end
 end)
 
 -- When a character dies, check if I was the last one to do damage on him and displays on the screen as a kill
 Character:Subscribe("Death", function(character, last_damage_taken, last_bone_damaged, damage_type_reason, hit_from_direction, instigator)
-	if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
-
 	local player = character:GetPlayer()
 
 	local name = "BOT"
+	local killer_name = "BOT"
 
 	-- Attempts to get the player's name (if possessed by one)
 	if (player) then
 		name = player:GetName()
 	end
 
-    -- Plays a sound kill feedback
-	Sound(Vector(), "NanosWorld::A_Kill_Feedback", true)
+	if (instigator) then
+		killer_name = instigator:GetName()
+	end
 
 	-- Gets the lat hit bone and check if it was a Headshot
 	local is_headshot = last_bone_damaged == "head" or last_bone_damaged == "neck_01"
+	local is_suicide = instigator == player
 
-	KillHUDUI:CallEvent("AddKill", {name, is_headshot})
+	KillHUDUI:CallEvent("AddKillNotification", { name, killer_name, is_headshot, is_suicide })
+
+	if (instigator ~= NanosWorld:GetLocalPlayer()) then return end
+
+	if (NanosWorld:GetLocalPlayer():GetControlledCharacter() == character) then
+		return
+	end
+
+    -- Plays a sound kill feedback
+	Sound(Vector(), "NanosWorld::A_Kill_Feedback", true)
+
+	if (is_headshot) then
+		if (KillHUDUIConfiguration.enable_autoscore) then
+			AddScore(KillHUDUIConfiguration.headshot_score, "headshot", "HEADSHOT", false)
+		end
+	end
+
+	AddKill(name, is_headshot, KillHUDUIConfiguration.kill_score)
+end)
+
+-- Event for configuring the Kill HUD
+Events:Subscribe("ConfigureBattlefieldKillUI", function(enable_autoscore, kill_score, headshot_score)
+	KillHUDUIConfiguration.enable_autoscore = enable_autoscore
+	KillHUDUIConfiguration.kill_score = kill_score
+	KillHUDUIConfiguration.headshot_score = headshot_score
+end)
+
+-- On Tick, updates all HitMarks
+Client:Subscribe("Tick", function(delta_time)
+	for k, h in ipairs(HitMarks) do
+		h.cooldown = h.cooldown - delta_time * 1000
+		if (h.cooldown <= 0) then
+			KillHUDUI:CallEvent("UpdateDamageIndicator", {h.id, false})
+			table.remove(HitMarks, k)
+		else
+			local position2D = Render:Project(h.location)
+			KillHUDUI:CallEvent("UpdateDamageIndicator", {h.id, true, position2D.X, position2D.Y})
+		end
+	end
 end)
--- Client/UI/index.js
+++ Client/UI/index.js
@@ -3,6 +3,37 @@ var interval_score;
 var total_score = 0;
 var total_score_current = 0;
 
+
+function CalculateAngleBetweenPoints(x0, y0, x1, y1) {
+	return Math.atan2(y1 - y0, x1 - x0) * 180 / Math.PI;
+}
+
+Events.Subscribe("UpdateDamageIndicator", function(id, enable, posX, posY) {
+	let element = $(`#${id}`);
+
+	if (enable) {
+		if (!element.length) {
+			element = $(`<span id='${id}' class='damage_indicator'>`);
+			$("#damage_indicators").append(element);
+		}
+		
+		let angle = 0;
+		if (posX == -1 && posY == -1) {
+			angle = 180;
+		} else {
+			const screen_width_center = window.innerWidth / 2;
+			const screen_height_center = window.innerHeight - window.innerHeight / 3;
+		
+			angle = CalculateAngleBetweenPoints(screen_width_center, screen_height_center, posX, posY) + 90;
+		}
+
+		element.css("transform", `translate(-50%, -50%) rotate(${angle}deg)`);
+	} else {
+		if (element.length)
+			element.fadeOut(500, function() { $(this).remove(); });
+	}
+});
+
 Events.Subscribe("AddScore", function(score, type_id, label, use_current) {
 	total_score += score;
 
@@ -13,7 +44,7 @@ Events.Subscribe("AddScore", function(score, type_id, label, use_current) {
 	if (use_current)
 	{
 		// Try to find if there is already a score item
-		let score_feed_item_value = $(`.${type_id} .score_value_item`);
+		const score_feed_item_value = $(`.${type_id} .score_value_item`);
 
 		// If a score item is found, then update it and return, otherwise it will keep on and will create a new one
 		if (score_feed_item_value.length)
@@ -24,24 +55,55 @@ Events.Subscribe("AddScore", function(score, type_id, label, use_current) {
 	}
 
 	// Create a new score item and prepend it on the list
-	let score_feed_item = $(`<span class='score_feed_item ${type_id}'>${label} +<span class='score_value_item'>${score}</span></span>`);
+	const score_feed_item = $(`<span class='score_feed_item ${type_id}'>${label} +<span class='score_value_item'>${score}</span></span>`);
 	$("#score_feed").prepend(score_feed_item);
 });
 
-Events.Subscribe("AddKill", function(name, is_headshot) {
-	// Adds 20 score for killing
-	total_score += 20;
+Events.Subscribe("AddKill", function(name, is_headshot, score) {
+	// Adds score for killing
+	total_score += score;
 
 	// Displays the killed name
-	$("#death_name").html(`${name} +20`);
+	$("#death_name").html(`${name} +${score}`);
 
 	// If it was headshot, displays the red skull, otherwise displays the white
-	let death_count_white = $(`<span class='death_count ${is_headshot ? "death_count_red" : "death_count_white"}'>`);
+	const death_count_white = $(`<span class='death_count ${is_headshot ? "death_count_red" : "death_count_white"}'>`);
 	$("#death_counts").prepend(death_count_white);
 
 	// Displays all DOM elements
 	DisplayHUD();
 });
+
+Events.Subscribe("AddKillNotification", function(dead_name, killer_name, is_headshot, is_suicide) {
+	const kill_notification_item = $(`<span class='kill_notification'>`);
+
+	if (!is_suicide) {
+		const kill_notification_killer = $(`<span class='kill_notification_killer'>`);
+		kill_notification_killer.html(killer_name);
+		kill_notification_item.append(kill_notification_killer);
+	}
+
+	const kill_notification_action = $(`<span class='kill_notification_action'>`);
+	kill_notification_action.html(is_suicide ? "suicided" : "killed");
+	kill_notification_item.append(kill_notification_action);
+	
+	if (is_headshot) {
+		const kill_notification_headshot = $(`<span class='kill_notification_headshot'>`);
+		kill_notification_item.append(kill_notification_headshot);
+	}
+	
+	const kill_notification_dead = $(`<span class='kill_notification_dead'>`);
+	kill_notification_dead.html(dead_name);
+	kill_notification_item.append(kill_notification_dead);
+
+	$("#kill_notifications").prepend(kill_notification_item);
+
+	// Destroys the entry after 10 seconds
+	setTimeout(function(span) {
+		span.remove();
+	}, 10000, kill_notification_item);
+});
+
 
 // Resets all animations and displays the HUD, also resets the timers and creates a new one to hide it in 4 seconds
 function DisplayHUD() {
@@ -84,7 +146,7 @@ function ResetHUD() {
 
 // Updates the HUD, i.e. updates the current displayed score in the screen
 function UpdateHUD() {
-	let interp_pace = Math.max((total_score - total_score_current) / 2, 1);
+	const interp_pace = Math.max((total_score - total_score_current) / 2, 1);
 
 	total_score_current = parseInt(Math.min(total_score, total_score_current + interp_pace));
 	$("#total_score").html(total_score_current);
